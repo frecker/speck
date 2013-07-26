f8c54b6a390270eb2d962fe3de3b3be02089b12b
Frank Recker <frank.recker@arcor.de>
2013-07-26 22:14:20 +0200

    encryption is done without any padding

--- Speck.hs
+++ Speck.hs
@@ -24,7 +24,7 @@ ctr_file_encrypt f g = do
   in_handle <- openBinaryFile f ReadMode
   out_handle <- openBinaryFile g WriteMode
   xs <- B.hGetContents in_handle
-  B.hPut out_handle $ B.pack $ ctr_encrypt [key_1,key_0] $ B.unpack xs
+  B.hPut out_handle $ B.pack $ ctr_encrypt key_1 key_0 $ B.unpack xs
   hClose out_handle
   hClose in_handle
 
@@ -33,7 +33,7 @@ ctr_file_decrypt f g = do
   in_handle <- openBinaryFile f ReadMode
   out_handle <- openBinaryFile g WriteMode
   xs <- B.hGetContents in_handle
-  B.hPut out_handle $ B.pack $ ctr_decrypt [key_1,key_0] $ B.unpack xs
+  B.hPut out_handle $ B.pack $ ctr_decrypt key_1 key_0 $ B.unpack xs
   hClose out_handle
   hClose in_handle
 
@@ -68,42 +68,21 @@ random_word64 g =
     o = maxBound
 
 ---- Real World encryption and decryption
-ctr_encrypt :: [Word64] -> [Word8] -> [Word8]
-ctr_encrypt key xs =
-  concat $ map word64_to_word8 zs
-  where
-    zs = zip_tupel_xor ys ctr_stream
-    zip_tupel_xor ((u,v):ws) ((u',v'):ws') =
-      (u `xor` u'):(v `xor` v'):zip_tupel_xor ws ws'
-    zip_tupel_xor [] _ = []
-    ctr_stream = [encryption rks ch cl | ch<-[0..maxBound], cl<-[0..maxBound]]
-    ys = help $ padding 16 1 0 xs
-    help (u:us) = let (y,z) = splitAt 8 u
-                  in  (word8_to_word64 y,word8_to_word64 z):help us
-    help [] = []
-    rks = key_expansion key
+ctr_encrypt :: Word64 -> Word64 -> [Word8] -> [Word8]
+ctr_encrypt l0 k0 xs =
+  zipWith xor xs (ctr_stream l0 k0)
 
-ctr_decrypt :: [Word64] -> [Word8] -> [Word8]
-ctr_decrypt key xs =
-  de_padding "ctr_decrypt" 16 1 0 $ concat $ map word64_to_word8 zs
-  where
-    zs = zip_tupel_xor ys ctr_stream
-    zip_tupel_xor ((u,v):ws) ((u',v'):ws') =
-      (u `xor` u'):(v `xor` v'):zip_tupel_xor ws ws'
-    zip_tupel_xor [] _ = []
-    ctr_stream = [encryption rks ch cl | ch<-[0..maxBound], cl<-[0..maxBound]]
-    ys = help $ blocks 16 xs
-    help (u:us) = let (y,z) = splitAt 8 u
-                  in  (word8_to_word64 y,word8_to_word64 z):help us
-    help [] = []
-    rks = key_expansion key
+ctr_decrypt :: Word64 -> Word64 -> [Word8] -> [Word8]
+ctr_decrypt = ctr_encrypt
 
-word8_to_word64 :: [Word8] -> Word64
-word8_to_word64 xs =
-  help 8 0 xs
+ctr_stream :: Word64 -> Word64 -> [Word8]
+ctr_stream l0 k0 =
+  w8_stream
   where
-    help 0 acc [] = acc
-    help n acc (y:ys) = help (n-1) (256 * acc + fromIntegral y) ys
+    w8_stream =
+      concat [word64_to_word8 u ++ word64_to_word8 v | (u,v) <- w64_stream]
+    w64_stream = [encryption rks ch cl | ch<-[0..maxBound], cl<-[0..maxBound]]
+    rks = key_expansion l0 k0
 
 word64_to_word8 :: Word64 -> [Word8]
 word64_to_word8 x =
@@ -113,36 +92,6 @@ word64_to_word8 x =
     help n x = let y = fromIntegral (mod x 256)
                    z = (div x 256)
                in  y:help (n-1) z
-
-padding n a b xs =
-  help n xs
-  where
-     help m (y:ys)
-       | m>0  = let (zs:zss) = help (m-1) ys in ((y:zs):zss)
-       | m==0 = []:help n (y:ys)
-     help m []
-       | m>0  = [a:take (m-1) (repeat b)]
-       | m==0 = []:[a:take (n-1) (repeat b)]
-
-de_padding s n a b xs =
-  help xs
-  where
-    help ys
-      | not (null ws) = zs ++ help ws
-      | length zs==n = help2 (reverse zs)
-      where
-        (zs,ws) = splitAt n ys
-    help2 (y:ys)
-      | y==b = help2 ys
-      | y==a = reverse ys
-      | otherwise = error "last block has wrong padding"
-
-blocks n [] = []
-blocks n xs
-  | length ys == n = ys:blocks n zs
-  | otherwise = error "last block has wrong size"
-  where
-    (ys,zs) = splitAt n xs
 
 ---- Spec128/128 - implemented with Word64
 -- Tests the test vector
@@ -154,7 +103,8 @@ self_test
     keys = ["0f0e0d0c0b0a0908","0706050403020100"]
     plain = ("6c61766975716520","7469206564616d20")
     cipher = ("a65d985179783265","7860fedf5c570d18")
-    rks = key_expansion $ map hex_string_to_num keys
+    rks = key_expansion l0 k0
+    [l0,k0] = map hex_string_to_num keys
     cipher' = num_tup_to_hex $ uncurry (encryption rks) (hex_tup_to_num plain)
     plain' = num_tup_to_hex $ uncurry (decryption (reverse rks)) (hex_tup_to_num cipher')
     hex_tup_to_num (x,y) = (hex_string_to_num x,hex_string_to_num y)
@@ -162,8 +112,8 @@ self_test
 
 -- keys are in the order 1 0
 -- round keys are in order 0..T-1
-key_expansion :: [Word64] -> [Word64]
-key_expansion [l0,k0] =
+key_expansion :: Word64 -> Word64 -> [Word64]
+key_expansion l0 k0 =
   take 32 $ help 0 l0 k0
   where
     help i li ki = let i' = i+1
